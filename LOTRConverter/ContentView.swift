818651e4c6381ee7f4f58032bbcee449dbbfb04f
Kiyegga Francis <kiyeggafrancis1@gmail.com>
2024-12-11 05:39:39 +0300

    info image and spacer was add to our app

--- LOTRConverter/ContentView.swift
+++ LOTRConverter/ContentView.swift
@@ -9,13 +9,93 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            //backgroung image
+            Image(.background)
+                .resizable()
+                .ignoresSafeArea()
+                
+            
+            VStack {
+                //prancing pony image view
+                Image(.prancingpony)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: 200)
+                //currency exchange text view
+                Text("Currency Exchange")
+                    .font(.largeTitle)
+                    .foregroundStyle(.white)
+                
+                
+                //convertion section
+                HStack {
+                    //Left conversion section
+                    
+                    VStack {
+                        // currency
+                        
+                         HStack {
+                            
+                            //currency image
+                             Image(.silverpiece)
+                                 .resizable()
+                                 .scaledToFit()
+                                 .frame(height: 33 )
+                             
+                            //currency text
+                             Text("Silver Piece")
+                                 .font(.headline)
+                                 .foregroundStyle(.white)
+                        }
+                        
+                        //text feild
+                        Text("Text Field")
+                    }
+                    
+                    //equal sign
+                    Image(systemName: "equal")
+                        .font(.largeTitle)
+                        .foregroundStyle(.white)
+                        .symbolEffect(.pulse)
+                    
+                   // Right conversion section
+                    
+                    VStack {
+                        // currency
+                        
+                         HStack {
+                            
+                            //currency text
+                             Text("Gold Piece")
+                                 .font(.headline)
+                                 .foregroundStyle(.white)
+                             
+                            //currency image
+                             Image(.goldpiece)
+                                 .resizable()
+                                 .scaledToFit()
+                                 .frame(height: 33)
+                                 
+                        }
+                        
+                        //text feild
+                        Text("Text Field")
+                    }
+                }
+                Spacer()
+                
+                //info button
+                Image(systemName: "info.circle.fill")
+                    .font(.largeTitle)
+                    .foregroundStyle(.white)
+                    
+                
+                
+            }
+            //.border(.blue)
         }
-        .padding()
+
     }
 }
 
